5fc3924784f4966a5d0ab9e22b42440771602dfd
pvds <pvdsteen@gmail.com>
2017-06-04 21:55:06 +0200

    Setup event listeners and function/plugin initiation
    - remove redundant initialize file

--- app/src/scripts/app.js
+++ app/src/scripts/app.js
@@ -1,6 +1,73 @@
+/**
+ * TODO: separate logic into main.js and theme.js
+ **/
 (function () {
-    var doc = document.documentElement;
 
-    doc.classList.remove('no-js');
-    doc.classList.add('js');
+    document.addEventListener('DOMContentLoaded', function() {
+        /**
+         * Set global variables
+         **/
+        moira.initGlobalVars();
+
+        /**
+         * Initialize smooth anchor scrolling
+         **/
+        initSmoothScrolling();
+
+        /**
+         * Initialize sticky elements
+         **/
+        new Sticky('.sticky');
+
+        /**
+         * Animate Elements
+         */
+        if(moira.progress.animation && !moira.vars.mobile) {
+            appearElems(moira.vars.animateEl, 150);
+        }
+
+        /**
+         * Circle & Line Charts
+         */
+        if(!moira.progress.animation || moira.vars.mobile) {
+            manualProgressTrigger();
+        }
+
+        /**
+         * Listen for print actions
+         */
+        var mediaQueryList = window.matchMedia('print');
+        mediaQueryList.addListener(function(mql) {
+            if (mql.matches) {
+                console.log('onbeforeprint equivalent');
+                manualProgressTrigger();
+            } else {
+                console.log('onafterprint equivalent');
+            }
+        });
+
+        /**
+         * Initiate scrollspy
+         * */
+        gumshoe.init(moira.scrollspy);
+
+        /**
+         * Initiate expander
+         * */
+        expander();
+    });
+
+    window.addEventListener('resize', function() {
+        // Re Init Vars
+        moira.vars.windowW = window.innerWidth;
+        moira.vars.windowH = window.innerHeight;
+        moira.vars.windowScrollTop = window.pageYOffset;
+    });
+
+    window.addEventListener('scroll', function() {
+        moira.vars.windowScrollTop = window.pageYOffset;
+    });
+
+    window.addEventListener('load', function() {
+    });
 }());
