404034c2f1ca49294aec202999449c99fe355445
pvdsteen <pvdsteen@gmail.com>
2025-10-04 22:07:14 +0200

    feat: add soft skills and interests to about me section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,7 +12,7 @@ let { about, experience, education, references, knowledge } = data.content;
 		<Section title={about.personal.title} size="sm">
 			<dl class="grid grid-cols-[100px_1fr] gap-2">
 			{#each about.personal.definitions as definition}
-				<dt class="font-bold">{definition.term}</dt>
+				<dt class="font-semibold">{definition.term}</dt>
 				<dd class="text-black-light">{@html definition.def}</dd>
 			{/each}
 			</dl>
@@ -23,17 +23,71 @@ let { about, experience, education, references, knowledge } = data.content;
 				{@const ariaValue = Number(language.ariaValue)}
 				{@const value = Number(language.value)}
 				<li>
-					<strong class="block mb-2">{language.title}</strong>
-					<span role="progressbar" aria-valuemax="100" aria-valuemin="0"
+					<strong class="block font-semibold mb-2">{language.title}</strong>
+					<div role="progressbar" aria-valuemax="100" aria-valuemin="0"
 						 aria-valuenow={ariaValue} class="flex items-center gap-2">
 					{#each Array(10) as _, i}
 						<span
 							class="w-4 h-4 border-1 border-primary rounded-full {i <= value ?
 							'bg-primary' : ''}"></span>
 					{/each}
-					</span>
+					</div>
 				</li>
 			{/each}
+			</ul>
+		</Section>
+		<Section title={about.soft.title} size="sm">
+			<ul class="flex flex-col gap-4">
+			{#each about.soft.skills as skill}
+				{@const ariaValue = Number(skill.ariaValue)}
+				<li>
+					<div class="flex justify-between items-center mb-2">
+						<strong class="font-semibold">{skill.title}</strong>
+						<p class="text-sm font-light text-black-light">{skill.text}</p>
+					</div>
+					<div
+						role="progressbar"
+						aria-valuemin="0"
+						aria-valuemax="100"
+						aria-valuenow={ariaValue}
+						style={`--p:${ariaValue}%`}
+						class="
+							relative h-1.5 w-full rounded-full bg-black overflow-hidden
+							after:content-[''] after:absolute after:inset-0
+							after:w-[var(--p)] after:h-full after:rounded-full
+							after:bg-primary
+							after:transition-[width] after:duration-500 after:ease-out
+						"
+					></div>
+				</li>
+			{/each}
+			</ul>
+		</Section>
+		<Section title={about.soft.title} size="sm">
+			<ul class="flex flex-col gap-4">
+				{#each about.interests.skills as skill}
+					{@const ariaValue = Number(skill.ariaValue)}
+					<li>
+						<div class="flex justify-between items-center mb-2">
+							<strong class="font-semibold">{skill.title}</strong>
+							<p class="text-sm font-light text-black-light">{skill.text}</p>
+						</div>
+						<div
+							role="progressbar"
+							aria-valuemin="0"
+							aria-valuemax="100"
+							aria-valuenow={ariaValue}
+							style={`--p:${ariaValue}%`}
+							class="
+							relative h-1.5 w-full rounded-full bg-black overflow-hidden
+							after:content-[''] after:absolute after:inset-0
+							after:w-[var(--p)] after:h-full after:rounded-full
+							after:bg-primary
+							after:transition-[width] after:duration-500 after:ease-out
+						"
+						></div>
+					</li>
+				{/each}
 			</ul>
 		</Section>
 	</div>
